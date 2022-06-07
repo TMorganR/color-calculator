9a732f94ac9aeff97043b52ad052ffc8b9adcbad
TMorganR <105664184+TMorganR@users.noreply.github.com>
2022-06-07 10:49:13 -0500

    Add files via upload

--- color-calculator.js
+++ color-calculator.js
@@ -4,17 +4,17 @@ let one = "color combination"
 let two = "color deconstruction"
 let userChoice1 = (prompt("This is color calculator, 'one' is to combine and 'two' is to deconstruct. Enter your choice: "))
 
-if (userChoice1 == "one"){
+if (userChoice1 === "one"){
     let color1 = (prompt("choose first color: "))
     let color2 = (prompt("choose second color: "))
 
-    if(color1 == "red" && color2 === "blue" || color1 == "blue" && color2 == "red"){
+    if(color1 === "red" && color2 === "blue" || color1 === "blue" && color2 === "red"){
     console.log("purple")
     }
-    else if (color1 == "red" && color2 == "yellow" || color1 == "yellow" && color2 == "red"){
+    else if (color1 === "red" && color2 === "yellow" || color1 === "yellow" && color2 === "red"){
     console.log("orange")
     } 
-    else if(color1 == "blue" && color2 == "yellow" || color1 == "yellow" && color2 == "blue"){
+    else if(color1 === "blue" && color2 === "yellow" || color1 === "yellow" && color2 === "blue"){
     console.log("green")    
     }
     else{
@@ -22,21 +22,22 @@ if (userChoice1 == "one"){
     }
 }
 
-if (userChoice1 == "two"){
+if (userChoice1 === "two"){
     let color3 = (prompt("What color do you want to deconstruct?"))
     
-    if(color3 == "purple"){
+    if(color3 === "purple"){
         console.log("purple comes from red + blue")
     }
-    else if(color3 == "orange"){
+    else if(color3 === "orange"){
         console.log("orange comes from red + yellow")
     }
-    else if(color3 == "green"){
+    else if(color3 === "green"){
         console.log("green comes from blue + yellow")
     }
     else{
         console.log("error")
     }
+}
 
 
 
